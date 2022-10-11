f16693f8ca0725a26c2fb44fe42591deee07a5fd
yuriynemcikov <yuriy1993@inbox.ru>
2022-10-11 18:27:38 +0300

    first securityApp

---
+++ src/test/java/nemchikov/firstSecurity/FirstSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package nemchikov.firstSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
