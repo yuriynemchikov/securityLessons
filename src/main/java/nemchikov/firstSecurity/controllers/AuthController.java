38198666a76f9f4ecbf0bb1a702490e30c5ab83f
yuriynemcikov <yuriy1993@inbox.ru>
2022-10-13 17:16:43 +0300

    authorization with Spring Security

---
+++ src/main/java/nemchikov/firstSecurity/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package nemchikov.firstSecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "auth/login";
+    }
+
+}
